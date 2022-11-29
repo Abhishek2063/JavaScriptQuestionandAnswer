685c2d5f597d6b8016ccccb986f1fee5ce6ee0e1
vikramvermatg <sales18@talentelgia.com>
2022-11-29 12:37:52 +0530

    javasrcipt question solved

---
+++ sum_of_two_numbers.js
@@ -0,0 +1,8 @@
+const addition = (a, b) => {
+    let c = a + b;
+    return c;
+}
+console.log(addition(5, 3));
+console.log(addition(-5, -3));
+console.log(addition(5, -3));
+console.log(addition(-5, 3));
